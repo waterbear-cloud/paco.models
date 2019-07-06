7b4aae24f30ab3d846dddffec33106e16fbfa325
Kevin Teague <kevin.t@waterbear.cloud>
2019-07-06 12:07:58 -0700

    Back to development: 1.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('CHANGELOG.md') as f:
 
 setup(
     name='aim.models',
-    version='1.1.0',
+    version='1.1.1.dev0',
     description='aim.models: Semantic cloud infrastructure configuration file format and object model',
     author='Waterbear Cloud',
     author_email='hello@waterbear.cloud',

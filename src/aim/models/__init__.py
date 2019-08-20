0ce05db73ca2029da5b454b6345c94824683b4c8
Kevin Teague <kevin.t@waterbear.cloud>
2019-08-19 18:57:38 -0700

    Add deepcopy_except_parent

--- src/aim/models/__init__.py
+++ src/aim/models/__init__.py
@@ -1,3 +1,4 @@
+import copy
 from aim.models.loader import ModelLoader
 
 
@@ -9,3 +10,14 @@ def load_project_from_yaml(path, config_processor=None):
     loader = ModelLoader(path, config_processor)
     loader.load_all()
     return loader.project
+
+def deepcopy_except_parent(obj):
+    """Returns a deepcopy on on the object, but does not recurse up the
+    __parent__ attribute to prevent copying the entire model.
+    """
+    parent = obj.__parent__
+    obj.__parent__ = None
+    copy_obj = copy.deepcopy(obj)
+    obj.__parent__ = parent
+    return copy_obj
+

abf66db9c4368bf8cce578c8c3c723686434a4ff
Kevin Teague <kevin.t@waterbear.cloud>
2019-11-28 15:31:49 +0200

    Add a legacy flag for aim_name_2019_11_28

--- src/paco/models/schemas.py
+++ src/paco/models/schemas.py
@@ -207,7 +207,8 @@ valid_legacy_flags = (
         'iam_user_default_password_2019_10_12',
         'netenv_loggroup_name_2019_10_13',
         'route53_record_set_2019_10_16',
-        'target_group_name_2019_10_29'
+        'target_group_name_2019_10_29',
+        'aim_name_2019_11_28'
     )
 class InvalidLegacyFlag(schema.ValidationError):
     __doc__ = 'Not a valid legacy flag. Must be one of: '
